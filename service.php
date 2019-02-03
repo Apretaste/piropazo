0700cf28a12b17440e9a554435d97c2664146407
Salvi Pascual <salvi.pascual@gmail.com>
2019-02-03 18:37:55 -0500

    cretea functions file

--- service.php
+++ service.php
@@ -752,36 +752,4 @@ class Service
 
 		return $percentage;
 	}
-
-	/**
-	 * Function executed when a payment is finalized
-	 * Add new flowers and crowns to the database
-	 *
-	 * @author salvipascual
-	 * @param Payment $payment
-	 * @return boolean
-	 */
-	public function payment(Payment $payment)
-	{
-		// get the number of articles purchased
-		$flowers = 0; $crowns = 0;
-		if($payment->code == "FLOWER") $flowers = 1;
-		if($payment->code == "CROWN") $crowns = 1;
-		if($payment->code == "PACK_ONE") {$flowers = 7; $crowns = 2;}
-		if($payment->code == "PACK_TWO") {$flowers = 15; $crowns = 4;}
-
-		// do not allow wrong codes
-		if($flowers + $crowns <= 0) return false;
-
-		// get the person Id
-		$personId = Utils::personExist($payment->buyer);
-
-		// save the articles in the database
-		Connection::query("
-			UPDATE _piropazo_people
-			SET flowers = flowers+$flowers, crowns = crowns+$crowns
-			WHERE id_person = $personId");
-
-		return true;
-	}
 }
